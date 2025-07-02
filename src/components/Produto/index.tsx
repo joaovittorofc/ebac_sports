1b8e30425868edeb07ab3e4311732d6a0cb4e3ed
joaovittormkt <joaovittorvieira15@gmail.com>
2025-07-01 23:30:54 -0300

    Update index.tsx

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -1,9 +1,11 @@
 import { Produto as ProdutoType } from '../../App'
 import * as S from './styles'
 
+import { useDispatch } from 'react-redux'
+import { addItem } from '../../store/cartSlice'
+
 type Props = {
   produto: ProdutoType
-  aoComprar: (produto: ProdutoType) => void
   favoritar: (produto: ProdutoType) => void
   estaNosFavoritos: boolean
 }
@@ -15,10 +17,11 @@ export const paraReal = (valor: number) =>
 
 const ProdutoComponent = ({
   produto,
-  aoComprar,
   favoritar,
   estaNosFavoritos
 }: Props) => {
+  const dispatch = useDispatch()
+
   return (
     <S.Produto>
       <S.Capa>
@@ -33,7 +36,12 @@ const ProdutoComponent = ({
           ? '- Remover dos favoritos'
           : '+ Adicionar aos favoritos'}
       </S.BtnComprar>
-      <S.BtnComprar onClick={() => aoComprar(produto)} type="button">
+      <S.BtnComprar
+        onClick={() =>
+          dispatch(addItem({ ...produto, quantidade: 1 }))
+        }
+        type="button"
+      >
         Adicionar ao carrinho
       </S.BtnComprar>
     </S.Produto>
